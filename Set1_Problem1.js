65c4196f088b48663b43dd8c52ddfb210d251bb7
VolodymyrNorth <126442437+VolodymyrNorth@users.noreply.github.com>
2023-03-09 17:08:08 -0800

    Update Set1_Problem1.js

--- Set1_Problem1.js
+++ Set1_Problem1.js
@@ -3,9 +3,9 @@ const list = [5, 3, 2, 1, 16, 15, 7]; //creating an array of integers
 
 function logEvens(arr)      //creating a function that would print all even elements of array arr
 {                            
-    for(let i = 0; i < list.length; i++)  //looping through an array
-        if(list[i] % 2 == 0)      //checking whether the number is even
-            console.log(list[i] + " ");      //printing the element if it is
+    for(let i = 0; i < arr.length; i++)  //looping through an array
+        if(arr[i] % 2 == 0)      //checking whether the number is even
+            console.log(arr[i] + " ");      //printing the element if it is
 }
 
 logEvens(list);     
